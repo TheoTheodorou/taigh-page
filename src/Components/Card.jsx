19a8e7ea8f29135e8bdcc3517f6e814b1c531011
Theo Theodorou <theodore.theodorou@mac.com>
2023-04-24 16:52:06 +0100

    Card links

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Card(props) {
   return (
     <div className="group relative">
       <div className="  relative h-auto w-full overflow-hidden rounded-lg bg-white group-hover:opacity-75 ">
-        <a href={props.href}>
+        <Link to={props.href}>
           <img
             src={props.link1}
             className="h-full w-full object-cover object-center"
+            to={props.href}
           />
-        </a>
+        </Link>
       </div>
       <img src={props.link2} className="w-full" />
     </div>
