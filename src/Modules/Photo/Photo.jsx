81d835b2a59096c8108aabe6dadc8d4d109c13db
Theo Theodorou <theodore.theodorou@mac.com>
2023-08-19 22:14:54 +0100

    Image Modal

--- src/Modules/Photo/Photo.jsx
+++ src/Modules/Photo/Photo.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+
 import PhotoAlbum from "react-photo-album";
 
 // Importing each photo explicitly
@@ -28,11 +29,13 @@ import photo23 from "/src/Resources/photo/23.jpg";
 import photo24 from "/src/Resources/photo/24.jpg";
 import photo25 from "/src/Resources/photo/25.jpg";
 
+import CloseIcon from "/src/Resources/assets/CloseIcon.jsx";
+
 const photos = [
   { src: photo1, width: 4504, height: 3378 },
   { src: photo2, width: 4504, height: 3378 },
   { src: photo3, width: 5184, height: 2614 },
-  { src: photo4, width: 4, height: 3 },
+  { src: photo4, width: 1528, height: 2037 },
   { src: photo5, width: 4672, height: 7008 },
   { src: photo6, width: 4672, height: 7008 },
   { src: photo7, width: 3840, height: 2160 },
@@ -57,17 +60,74 @@ const photos = [
 ];
 
 export default function Photo() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const modalRef = useRef(null);
+
+  const handleOutsideClick = (e) => {
+    if (e.target === modalRef.current) {
+      closeModal();
+    }
+  };
+
+  const handleImageClick = (imageSrc) => {
+    setSelectedImage(imageSrc);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedImage(null);
+  };
+
+  useEffect(() => {
+    if (isModalOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    // Cleanup function to reset the overflow property when the component is unmounted
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isModalOpen]);
+
   return (
-    <a href="https://www.google.com">
+    <div>
       <PhotoAlbum
         layout="rows"
         photos={photos}
         componentsProps={(containerWidth) => ({
           imageProps: {
             loading: (containerWidth || 0) > 600 ? "eager" : "lazy",
+            className: "hover:animate-pulse",
+            onClick: (event) => handleImageClick(event.target.src),
           },
         })}
       />
-    </a>
+
+      {isModalOpen && (
+        <div
+          ref={modalRef}
+          onClick={handleOutsideClick}
+          className="fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-black bg-opacity-75"
+        >
+          <div className="relative inline-block">
+            <img
+              src={selectedImage}
+              alt="Selected"
+              className="max-w-screen max-h-screen object-contain"
+            />
+            <button
+              className="absolute right-0 top-0 z-10 cursor-pointer p-4"
+              onClick={() => closeModal()}
+            >
+              <CloseIcon />
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
